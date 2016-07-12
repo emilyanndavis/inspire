5cf4fb63efb413c4e18f6655f0fec461b5d32652
Emily Davis <emilyanndavis@gmail.com>
2016-07-12 08:13:08 -0600

    changing jquery href to http

--- app/components/quote/quote-service.js
+++ app/components/quote/quote-service.js
@@ -1,6 +1,6 @@
 function QuoteService(){
-	var url = 'https://bcw-getter.herokuapp.com/?url=';
-	var url2 = 'https://quotesondesign.com/api/3.0/api-3.0.json';
+	var url = 'http://bcw-getter.herokuapp.com/?url=';
+	var url2 = 'http://quotesondesign.com/api/3.0/api-3.0.json';
 	var apiUrl = url + encodeURIComponent(url2);
 	//Do Not Edit above we have to go through the bcw-getter to access this api
 	
