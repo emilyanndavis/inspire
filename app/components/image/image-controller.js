11decb44d6259d9601e29f2281edc5841c0ddba9
Emily Davis <emilyanndavis@gmail.com>
2016-07-12 11:53:03 -0600

    re(?)parsing data from image api - fingers crossed

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -2,7 +2,7 @@
 	console.log('ready');
 	var imageService = new ImageService();
 	imageService.getImage(function(image){
-		console.log(image.url);
+		image = JSON.parse(image);
 		$('#wallpaper').css(`background-image`, `url(${image.url})`);
 		$('#wallpaper').css(`background-size`, `cover`);		
 	});
